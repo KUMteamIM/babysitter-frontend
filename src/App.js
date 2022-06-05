f9d05fa7b2becc474ab5d46bec9625116f60e99f
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2022-06-05 12:35:01 +0200

    Add suspense

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import '@lmu-med/styles/dist/lmu.css'
 import './App.scss';
 import CommonFooter from '@lmu-med/ci-components/dist/components/CommonFooter';
-import CommonCookieNotice from '@lmu-med/ci-components/dist/components/CommonCookieNotice';
 import { BrowserRouter as Router } from "react-router-dom";
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
@@ -18,12 +17,14 @@ function App() {
 
   return (
     <Provider store={store}>
+      <React.Suspense fallback="loading...">
       <Router>
         <AppHeader />
         <AppContent />
         <CommonFooter />
         {/* <CommonCookieNotice /> */}
       </Router>
+        </React.Suspense>
     </Provider>
   );
 }
